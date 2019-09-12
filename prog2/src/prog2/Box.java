e7cb81ded2bfa5d062c6d1fd1f2b05b5dfb0fb37
hussein421 <hussein90090@outlook.com>
2019-09-12 21:36:08 +0200

    klar med uppgifterna

--- prog2/src/prog2/Box.java
+++ prog2/src/prog2/Box.java
@@ -2,11 +2,8 @@ package prog2;
 
 public class Box {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		ujhiu
-		
+	Object[] input;
+	public Box(Object[] input) {
+		input=input;
 	}
-
 }
